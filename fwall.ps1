1526210650ec4c20237ac963868efb2ec49e2aca
jbs69420 <38573230+jbs69420@users.noreply.github.com>
2018-04-20 18:43:19 -0400

    Add files via upload

---
+++ fwall.ps1
@@ -0,0 +1,9 @@
+﻿netsh advfirewall firewall add rule name="Allow LDAP tcp" dir=in action=allow protocol=TCP localport=389
+netsh advfirewall firewall add rule name="Allow LDAP udp" dir=in action=allow protocol=UDP localport=389
+netsh advfirewall firewall add rule name="Allow LDAP tcp2" dir=in action=allow protocol=TCP localport=36
+netsh advfirewall firewall add rule name="Allow global catalog" dir=in action=allow protocol=TCP localport=3268
+netsh advfirewall firewall add rule name="Allow DNS tcp in" dir=in action=allow protocol=TCP localport=53
+netsh advfirewall firewall add rule name="Allow DNS udp in" dir=in action=allow protocol=UDP localport=53
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
+netsh advfirewall firewall add rule name="ICMP Allow outgoing V4 echo request" protocol=icmpv4:8,any dir=out action=allow
+netsh advfirewall firewall add rule name="Block all" dir=in action=drop protocol=ANY localport=any 
---
+++ gotham_5min.ps1
@@ -0,0 +1,11 @@
+﻿$inStr = Read-Host -Prompt 'Hit me with it: '
+$SecStr = ConvertTo-SecureString $inStr –asplaintext –force
+
+$userList = Get-LocalUser -Name *
+Foreach ($usr in $userList){
+    $usr | Set-LocalUser -Password $SecStr
+}
+
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 1 /f
+
+Invoke-Item (start powershell ((Split-Path $MyInvocation.InvocationName) + "\fwall.ps1"))
