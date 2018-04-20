b927b6c1be8fe9909cd00298c492035f0251e41f
jbs69420 <38573230+jbs69420@users.noreply.github.com>
2018-04-20 18:56:56 -0400

    Update fwall.ps1

--- fwall.ps1
+++ fwall.ps1
@@ -7,5 +7,5 @@ netsh advfirewall firewall add rule name="Allow DNS udp in" dir=in action=allow 
 netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
 netsh advfirewall firewall add rule name="ICMP Allow outgoing V4 echo request" protocol=icmpv4:8,any dir=out action=allow
 netsh advfirewall firewall add rule name="Allow HTTP" protocol=TCP,UDP dir=in action=allow localport=80
-netsh advfirewall firewall add rule name="Allow HTTP" protocol=TCP,UDP dir=in action=allow localport=443
+netsh advfirewall firewall add rule name="Allow HTTPS" protocol=TCP,UDP dir=in action=allow localport=443
 netsh advfirewall firewall add rule name="Block all" dir=in action=drop protocol=ANY localport=any 
